90f98ea32d9c9833dab9147ed2b466668a98c2dd
antonydellavecchia <antonydellavecchia@gmail.com>
2023-09-06 18:17:53 +0200

    can plot for 8, 16 and 32 sides

--- src/HyperbolicPlane.jl
+++ src/HyperbolicPlane.jl
@@ -1,119 +1,139 @@
-
-struct HyperbolicPlane{T<:Field, S <:Field}
-    base_field::T
-    field::S
+import Oscar.QQBar
+    
+function coordinates(z::qqbar)
+    return [real(z), imag(z)]
 end
 
-struct Geodesic{S <: FieldElem}
+function half_angle_coords(angle_coord::qqbar)
+    r = real(angle_coord)
+    cos = sqrt((1 + r) // 2)
+    sin = sqrt((1 - r) // 2)
+    
+    return cos + sqrt(QQBar(-1)) * sin
+end
+
+const PointMapType{T} = Pair{T, T} where T <: FieldElem
+function get_moebius(p_maps::Vector{<:PointMapType})
+    mat = Vector{qqbar}[]
+    for p_map in p_maps
+        z1 = p_map.first
+        z2 = p_map.second
+        eq = qqbar[z1, QQBar(1), - z1 * z2, -z2]
+        push!(mat, eq)
+    end
+    M = matrix(QQBar, mat)
+    dim, gen = kernel(M)
+    @req dim == 1 "cannot find moebius transformation"
+    
+    A = [gen[1] gen[2]; gen[3] gen[4]]
+    return A
+end
+
+################################################################################
+# Geodesic
+
+mutable struct Geodesic{S <: FieldElem}
     # points should be ordered counter clockwise
     p1::S
     p2::S
 end
 
+function get_circle_center(g::Geodesic)
+    v1 = coordinates(g.p1)
+    v2 = coordinates(g.p2)
+    
+    v1_perp = [-v1[2], v1[1]]
+    v2_perp = [-v2[2], v2[1]]
+    M = matrix(QQBar, hcat(v1_perp, v2_perp))
+    b = [v2[1] - v1[1], v2[2] - v1[2]]
+    t = inv(M) * b
+    result = t[1] .* v1_perp + v1
+
+    @req result == t[2] .* v1_perp + v1 "Error computing center"
+    i = sqrt(QQBar(-1))
+
+    return result[1] + i * result[2]
+end
+
+function get_arc(g::Geodesic)
+    # returns (radius, (angle1, angle2))
+    c = get_circle_center(g)
+    p1_c = g.p1 - c
+    p2_c = g.p2 - c
+    r = abs(p1_c)
+            
+    return r, (log_pi_i(p1_c // r), log_pi_i(p2_c // r))
+end
+
+function on_point(q::qqbar, M::Matrix{qqbar})
+    return (M[1, 1] * q + M[1, 2]) // (M[2, 1] * q + M[2, 2])
+end
+
+function on_geodesic(g::Geodesic{qqbar}, M::Matrix{qqbar}; copy::Bool = false)
+    p1 = on_point(g.p1, M)
+    p2 = on_point(g.p2, M)
+
+    if copy
+        return Geodesic(p1, p2)
+    end
+    g.p1 = p1
+    g.p2 = p2
+    return g
+end
+################################################################################
+# Fundamental Domain
 struct FundamentalDomain
-    H::HyperbolicPlane
     geodesics::Vector{Geodesic}
-    deck_transformations::Vector{Matrix{<:FieldElem}}
+    deck_transformations::Vector{Matrix{qqbar}}
 end
 
-function hyperbolic_plane()
-    Qx, x = QQ["x"]
-    F, _ = number_field(x^2 + 1)
-    return HyperbolicPlane(F, F)
-end
-
-base_field(H::HyperbolicPlane) = H.base_field
-field(H::HyperbolicPlane) = H.field
-
-function set_field(H::HyperbolicPlane, F::Field)
-    return HyperbolicPlane(base_field(H), F)
-end
-
-function fundamental_domain(n::UInt, H::Union{HyperbolicPlane, Nothing} = nothing)
-    if isnothing(H)
-        H = hyperbolic_plane()
-    end
-        
+function fundamental_domain(n::UInt)
     @req n % 4 == 0 "Can only divide boundary into multiples of 4"
-    n_halves = n // 4 - 1
-    i = gen(base_field(H))
-    angle_coord = i
-
-    for _ in 1:n_halves
-        H, angle_coord = half_angle_coords(H, angle_coord)
-    end
+    
+    angle_coord = exp_pi_i(QQBar(QQ(2 // n)))
 
     boundary_points = [angle_coord^k for k in 0:n - 1]
 
-    geodesics = Geodesic{elem_type(field(H))}[]
+    geodesics = Geodesic{qqbar}[]
 
     for k in 1:n
         push!(geodesics, Geodesic(boundary_points[k], boundary_points[(k % n) + 1]))
     end
 
-    deck_transformations = Matrix{<:FieldElem}[]
+    deck_transformations = Matrix{qqbar}[]
     n_2 = divexact(n, 2)
     for k in 1:n_2
         antipodal_k = k + n_2
-        t = deck_transformation(H, geodesics[k], geodesics[antipodal_k])
+        t = deck_transformation(geodesics[k], geodesics[antipodal_k])
         push!(deck_transformations, t)
     end
-    return FundamentalDomain(H, geodesics, deck_transformations)
+    return FundamentalDomain(geodesics, deck_transformations)
 end
 
-function deck_transformation(H::HyperbolicPlane, g1::Geodesic, g2::Geodesic)
-    fixed_point = (g2.p1 + g2.p2) // norm(g2.p1 + g2.p2)
+function deck_transformation(g1::Geodesic, g2::Geodesic)
+    fixed_point = (g2.p1 + g2.p2) // abs(g2.p1 + g2.p2)
     p_maps = [
         g1.p1 => g2.p2,
         g1.p2 => g2.p1,
         fixed_point => fixed_point
     ]
-    return get_moebius(H, p_maps)
+    m = get_moebius(p_maps)
+    @req on_point(fixed_point, m) == fixed_point "Point is not being fixed"
+    @req on_point(g1.p1, m) == g2.p2 "Error with mapping first point"
+    @req on_point(g1.p2, m) == g2.p1 "Error with mapping second point"
+
+    return m
 end
 
-function half_angle_coords(H::HyperbolicPlane, angle_coord::FieldElem)
-    base = base_field(H)
-    i = gen(base_field(H))
-    parent_field = parent(angle_coord)
-    Fx, x = parent_field["x"]
-    norm = absolute_norm(angle_coord)
-    @req isone(norm) "angle coordinate not on unit circle"
-    conjugate_coord = parent_field(norm) // angle_coord 
-    real = (angle_coord + conjugate_coord) // 2
-    q = x^2 - (1 + real) // 2 # cos
-    p = x^2 - (1 - real) // 2 # sin
-
-    if p == q
-        E, root_half = number_field(q)
-        H = set_field(H, E)
-        return H, root_half + root_half * i
-    elseif iszero(real^2 - 1//2)
-        E, a = number_field(q)
-        b = a // (1 + 2 * real)
-        H = set_field(H, E)
-        return H, a + b * i
-    end
-
-    E, (c, s) = number_field([q, p], check=false)
-    H = set_field(H, E)
-    return H, c + i * s
+################################################################################
+# Hyperbolic Plane
+struct HyperbolicPlane
+    D::FundamentalDomain
+    geodesics::Vector{Geodesic}
 end
 
-const PointMapType{T} = Pair{T, T} where T <: FieldElem
-function get_moebius(H::HyperbolicPlane, p_maps::Vector{<:PointMapType})
-    F = field(H)
-    mat = Vector{elem_type(F)}[]
-    for p_map in p_maps
-        z1 = p_map.first
-        z2 = p_map.second
-        eq = elem_type(F)[z1, F(1), - z1 * z2, -z2]
-        push!(mat, eq)
-    end
-    M = matrix(F, mat)
-    dim, gen = kernel(M)
-    @req dim == 1 "cannot find moebius transformation"
-    
-    A = [gen[1] gen[2]; gen[3] gen[4]];
-    d = (A[1, 1] * A[2, 2] - A[1, 2] * A[2, 1])
-    return [gen[1] // d  gen[2] // d; gen[3] gen[4]];
+function hyperbolic_plane(n::UInt)
+    return HyperbolicPlane(fundamental_domain(n), Geodesic[])
 end
+
+fundamental_domain(H::HyperbolicPlane) = H.D
--- src/plotting.jl
+++ src/plotting.jl
@@ -1,4 +1,77 @@
-@recipe function f(::HyperbolicPlane)
+# defines the global precision
+CC = AcbField(62)
+
+# Define the @recipe function for HyperbolicPlane
+@recipe function f(H::HyperbolicPlane)
     return Plots.partialcircle(0, 2π, 100, 1)
 end
 
+# Define the @recipe function for acb
+@recipe function f(::Type{acb}, c::acb)
+    return (convert(Float64, real(c)), convert(Float64, imag(c)))
+end
+
+@recipe function f(q::qqbar)
+    @series begin
+        [CC(q)]
+    end
+end
+
+@recipe function f(v::Vector{qqbar})
+    for q in v
+        @series begin
+            CC(q)
+        end
+    end
+end
+
+# Define the @recipe function for Vector{acb}
+@recipe function f(::Type{Vector{acb}}, v::Vector{acb})
+    return [(convert(Float64, real(c)), convert(Float64, imag(c))) for c in v]
+end
+
+@recipe function f(g::Geodesic{qqbar})
+    c = get_circle_center(g)
+    r, (θ1, θ2) = get_arc(g)
+
+    RR = ArbField(64)
+    θ1_f = convert(Float64, RR(θ1)) * π
+    θ2_f = convert(Float64, RR(θ2)) * π
+    
+    if θ1_f < 0
+        θ1_f += 2 * π
+    end
+    
+    radius = convert(Float64, RR(r))
+    points = Plots.partialcircle(θ1_f, θ2_f, 100, radius)
+    
+    c_real = convert(Float64, real(CC(c)))
+    c_imag = convert(Float64, imag(CC(c)))
+    [p .+ (c_real, c_imag) for p in points]
+end
+
+## Define the @recipe function for Vector{Geodesic}
+@recipe function f(v::Vector{Geodesic{qqbar}})
+    for g in v
+        @series begin
+            g
+        end
+    end
+end
+
+@recipe function f(H::HyperbolicPlane)
+    @series begin
+        Plots.partialcircle(0, 2π, 100, 1)
+    end
+    D = fundamental_domain(H)
+    @series begin
+        reduce(vcat, D.geodesics)
+    end
+
+    #for A in D.deck_transformations
+    #    @series begin
+    #        gs = [on_geodesic(g, A) for g in D.geodesics]
+    #        points = [[g.p1, g.p2] for g in gs]
+    #    end
+    #end
+end
